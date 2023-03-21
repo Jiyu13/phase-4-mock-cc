1435a96e701b14daab17f9428e745a5823e7f9e0
Ziru Zou <jiyucoding@yahoo.com>
2023-03-21 11:08:12 -0400

    add solution

--- server/app.py
+++ server/app.py
@@ -24,3 +24,152 @@ def home():
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+from flask import Flask, make_response, request, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Hero, Power, HeroPower
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+
+@app.route('/')
+def home():
+    return ''
+
+@app.route('/heroes', methods = ['GET'])
+def heroes():
+    heroes = Hero.query.all()
+    heroes_dict = [hero.to_dict(rules = ('-hero_powers',)) for hero in heroes]
+
+    response = make_response(
+        jsonify(heroes_dict),
+        200
+    )
+
+    return response
+
+@app.route('/heroes/<int:id>', methods = ['GET'])
+def heroByID(id):
+    hero = Hero.query.filter_by(id = id).first()
+
+    if hero:
+
+        hero_dict = hero.to_dict()
+
+        response = make_response(
+            jsonify(hero_dict),
+            200
+        )
+
+    else:
+
+        response = make_response(
+            {"error": "Hero not found"},
+            404
+        )
+
+    return response
+
+@app.route('/powers', methods = ['GET'])
+def powers():
+    powers = Power.query.all()
+    powers_dict = [power.to_dict(rules = ('-hero_powers',)) for power in powers]
+
+    response = make_response(
+        jsonify(powers_dict),
+        200
+    )
+
+    return response
+
+@app.route('/powers/<int:id>', methods = ['GET', 'PATCH'])
+def powerByID(id):
+    power = Power.query.filter_by(id = id).first()
+
+    if power:
+
+        if request.method == 'GET':
+
+            power_dict = power.to_dict()
+
+            response = make_response(
+                jsonify(power_dict),
+                200
+            )
+
+        elif request.method == 'PATCH':
+
+            try:
+
+                for key in request.get_json():
+                    setattr(power, key, request.get_json()[key])
+
+                db.session.add(power)
+                db.session.commit()
+
+                power_dict = power.to_dict()
+
+                response = make_response(
+                    jsonify(power_dict),
+                    200
+                )
+
+            except ValueError:
+
+                response = make_response(
+                    {"error": "Invalid input"},
+                    400
+                )
+
+    else:
+
+        response = make_response(
+            {"error": "Power not found"},
+            404
+        )
+
+    return response
+
+@app.route('/hero_powers', methods = ['POST'])
+def heroPowers():
+
+    try:
+
+        new_hero_power = HeroPower(
+            strength = request.get_json()['strength'],
+            hero_id = request.get_json()['hero_id'],
+            power_id = request.get_json()['power_id']
+        )
+
+        db.session.add(new_hero_power)
+        db.session.commit()
+
+        hero = Hero.query.filter(Hero.id == new_hero_power.hero_id).first()
+        hero_dict = hero.to_dict()
+
+        response = make_response(
+            jsonify(hero_dict),
+            201
+        )
+
+    except ValueError:
+
+        response = make_response(
+            {"error": "Invalid input"},
+            400
+        )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,66 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+db = SQLAlchemy(metadata = metadata)
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    serialize_rules = ('-hero_powers.hero', '-powers.heroes')
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
 
-# add any models you may need. 
+    hero_powers = db.relationship('HeroPower', backref = 'hero')
+
+    powers = association_proxy('hero_powers', 'power')
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
+
+    serialize_rules = ('-hero_powers.power', '-heroes.powers')
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    hero_powers = db.relationship('HeroPower', backref = 'power')
+
+    heroes = association_proxy('hero_powers', 'hero')
+
+    @validates('description')
+    def validates_description(self, key, description):
+        if not description or len(description) < 20:
+            raise ValueError("Strength must have a length")
+        return description
+
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    serialize_rules = ('-power.hero_powers', '-hero.hero_powers')
+
+    id = db.Column(db.Integer, primary_key = True)
+    strength = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    @validates('strength')
+    def validates_strength(self, key, strength):
+        if not strength == 'Strong' and not strength == 'Weak' and not strength == 'Average':
+            raise ValueError("Strength must be Strong, Weak, or Average")
+        return strength
